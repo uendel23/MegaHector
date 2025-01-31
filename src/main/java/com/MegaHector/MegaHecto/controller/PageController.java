68ca58bca05c0b260c0ac84ffa0d7f78c35db0f3
Uendel Nascimento <carinha23@gmail.com>
2025-01-31 11:23:51 -0300

    pageController

---
+++ src/main/java/com/MegaHector/MegaHecto/controller/PageController.java
@@ -0,0 +1,20 @@
+package com.MegaHector.MegaHecto.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    @ResponseBody
+    public ModelAndView home (){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home.html");
+        return modelAndView;
+    }
+}
